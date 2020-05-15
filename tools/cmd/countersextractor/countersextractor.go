241dc9cbbb830ecf537112d9f9903354b4686ba9
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2020-05-15 06:27:48 -0400

    First two tools for post-mortem analysis
    First two tools for post-mortem analysis: extract counters from old format profiles and analyse send counts

--- tools/cmd/countersextractor/countersextractor.go
+++ tools/cmd/countersextractor/countersextractor.go
@@ -8,33 +8,120 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
-func saveSendCounterLine() {
-
+type extractor struct {
+	numSendCountersBlocks int
+	numRecvCountersBlocks int
+	sendCountersFile      *os.File
+	recvCountersFile      *os.File
 }
 
-func saveRecvCounterLine() {
+func createExtractor(outputDir string) *extractor {
+	e := new(extractor)
 
+	e.numRecvCountersBlocks = 0
+	e.numSendCountersBlocks = 0
+
+	pathToSendCountersFile := filepath.Join(outputDir, "send_counters.txt")
+	pathToRecvCountersFile := filepath.Join(outputDir, "recv_counters.txt")
+
+	var err error
+	fmt.Printf("Writing to %s and %s\n", pathToRecvCountersFile, pathToSendCountersFile)
+	e.sendCountersFile, err = os.OpenFile(pathToSendCountersFile, os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		log.Printf("[ERROR] unable to open %s: %s", pathToSendCountersFile, err)
+		return nil
+	}
+	e.recvCountersFile, err = os.OpenFile(pathToRecvCountersFile, os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		log.Printf("[ERROR] unable to open %s: %s", pathToRecvCountersFile, err)
+		return nil
+	}
+
+	return e
 }
 
-func lineContextFromLine(line string) string {
+func (e *extractor) finalize() error {
+	e.recvCountersFile.Sync()
+	e.sendCountersFile.Sync()
+
+	e.recvCountersFile.Close()
+	e.sendCountersFile.Close()
+
+	return nil
+}
+
+func (e *extractor) saveSendCounterLine(line string) error {
+	if e == nil || e.sendCountersFile == nil {
+		return fmt.Errorf("invalid extractor")
+	}
+	_, err := e.sendCountersFile.WriteString(line + "\n")
+	if err != nil {
+		log.Printf("[ERROR] unable to write to file: %s", err)
+		return err
+	}
+	return nil
+}
+
+func (e *extractor) saveRecvCounterLine(line string) error {
+	if e == nil || e.recvCountersFile == nil {
+		return fmt.Errorf("invalid extractor")
+	}
+	_, err := e.recvCountersFile.WriteString(line + "\n")
+	if err != nil {
+		log.Printf("[ERROR] unable to write to file: %s", err)
+		return err
+	}
+	return nil
+}
+
+func lineIsCounterData(line string) bool {
+	if line == "" {
+		return false
+	}
+
+	if strings.HasPrefix(line, "#") {
+		return false
+	}
+
+	if strings.HasPrefix(line, "Total") {
+		return false
+	}
+
+	if strings.HasPrefix(line, "Rank") {
+		return false
+	}
+
+	return true
+}
+
+func (e *extractor) getContextFromLine(line string) string {
 	if strings.HasPrefix(line, "## Data sent per rank") {
+		e.numSendCountersBlocks++
+		e.sendCountersFile.WriteString("\n")
 		return "SEND"
 	}
 	if strings.HasPrefix(line, "## Data received per rank") {
+		e.numRecvCountersBlocks++
+		e.recvCountersFile.WriteString("\n")
 		return "RECV"
 	}
 	return ""
 }
 
-func readAndParseFileLinebyLine(filepath string) error {
+func readAndParseFileLinebyLine(filepath string, outputDir string) error {
+	e := createExtractor(outputDir)
+
 	file, err := os.Open(filepath)
 	if err != nil {
-		return fmt.Errorf("unable to open %f: %w", filepath, err)
+		return fmt.Errorf("unable to open %s: %w", filepath, err)
 	}
 	defer file.Close()
 
@@ -42,15 +129,21 @@ func readAndParseFileLinebyLine(filepath string) error {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		potentialCtxt := getContextFromLine(line)
+		potentialCtxt := e.getContextFromLine(line)
 		if potentialCtxt != "" {
 			ctxt = potentialCtxt
 		}
 		if lineIsCounterData(line) {
 			if ctxt == "SEND" {
-				err := saveSendCounterLine(line)
+				err := e.saveSendCounterLine(line)
+				if err != nil {
+					return err
+				}
 			} else {
-				err := saveRecvCounterLine(line)
+				err := e.saveRecvCounterLine(line)
+				if err != nil {
+					return err
+				}
 			}
 			if err != nil {
 				return fmt.Errorf("unable to save line: %w", err)
@@ -60,6 +153,13 @@ func readAndParseFileLinebyLine(filepath string) error {
 	if err := scanner.Err(); err != nil {
 		return fmt.Errorf("scanner failed while reading the file: %w", err)
 	}
+
+	e.finalize()
+
+	fmt.Printf("Number of send counters blocks read: %d\n", e.numSendCountersBlocks)
+	fmt.Printf("Number of recv counters blocks read: %d\n", e.numRecvCountersBlocks)
+
+	return nil
 }
 
 func main() {
@@ -68,9 +168,12 @@ func main() {
 
 	flag.Parse()
 
-	if file == "" || outputDir == "" {
+	if *file == "" || *outputDir == "" {
 		log.Fatalf("undefined input file or output directory")
 	}
 
-	readAndParseFileLinebyLine(file, outputDir)
+	err := readAndParseFileLinebyLine(*file, *outputDir)
+	if err != nil {
+		log.Fatalf("unable to parse input file: %s", err)
+	}
 }
--- tools/cmd/sendprofiler/sendprofiler.go
+++ tools/cmd/sendprofiler/sendprofiler.go
@@ -6,6 +6,225 @@
 
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type analyzer struct {
+	realDests map[int]int
+	ranksComm map[int]string
+}
+
+type rankData struct {
+	numZeroMsgs   int
+	ranksRealComm int
+	totalMsgs     int
+}
+
+func createAnalyzer() *analyzer {
+	a := new(analyzer)
+	a.realDests = make(map[int]int)
+	a.ranksComm = make(map[int]string)
+	return a
+}
+
+func (a *analyzer) finalize() error {
+	return nil
+}
+
+func (a *analyzer) resetStats() {
+	a.realDests = make(map[int]int)
+	a.ranksComm = make(map[int]string)
+
+	if len(a.realDests) > 0 || len(a.ranksComm) > 0 {
+		log.Fatalf("Map is not reset")
+	}
+}
+
+// parseForRanges parses the string and check for either a single value (a rank) or a range of ranks
+// The new rank is added appropriately
+func checkForRanges(str string, rank int, rankString string) (string, error) {
+	tokens := strings.Split(str, "-")
+	if len(tokens) > 1 {
+		// We have a range
+		lastRank, err := strconv.Atoi(tokens[1])
+		if err != nil {
+			return "", err
+		}
+		if lastRank+1 == rank {
+			tokens[1] = rankString
+			return strings.Join(tokens, "-"), nil
+		} else {
+			return str + ", " + rankString, nil
+		}
+	} else {
+		// no range of ranks at the end of the string
+		n, err := strconv.Atoi(str)
+		if err != nil {
+			return "", err
+		}
+		if n+1 == rank {
+			return str + "-" + rankString, nil
+		} else {
+			return str + ", " + rankString, nil
+		}
+	}
+}
+
+func (a *analyzer) parseLine(rank int, lineNum int, line string) (bool, error) {
+	var d rankData
+
+	tokens := strings.Split(line, " ")
+	if len(tokens) <= 1 {
+		// Not an actual line with counters
+		return false, nil
+	}
+
+	/*
+		if rank < 5 || rank >= 1024 {
+			fmt.Printf("I am rank %d at line %d and my counters are %s\n", rank, lineNum+1, line)
+		}
+	*/
+	for _, word := range tokens {
+		d.totalMsgs++
+		num, err := strconv.Atoi(word)
+		if err == nil {
+			if num == 0 {
+				d.numZeroMsgs++
+			} else {
+				//fmt.Printf("Rank %d has a non-zero count on line %d: count is at %d\n", rank, lineNum, num)
+				d.ranksRealComm++
+			}
+		}
+	}
+
+	//fmt.Printf("rank %d communicates with %d other ranks and has %d zero counts for a total of %d msgs\n", rank, d.ranksRealComm, d.numZeroMsgs, d.totalMsgs)
+
+	rankString := strconv.Itoa(rank)
+
+	finalStr := ""
+	if a.ranksComm[d.ranksRealComm] == "" {
+		finalStr = rankString
+	} else {
+		tokens := strings.Split(a.ranksComm[d.ranksRealComm], ", ")
+		if len(tokens) > 1 {
+			s, err := checkForRanges(tokens[len(tokens)-1], rank, rankString)
+			if err != nil {
+				return false, err
+			}
+			tokens[len(tokens)-1] = s
+			finalStr = strings.Join(tokens, ", ")
+		} else {
+			var err error
+			finalStr, err = checkForRanges(a.ranksComm[d.ranksRealComm], rank, rankString)
+			if err != nil {
+				return false, err
+			}
+		}
+	}
+	a.ranksComm[d.ranksRealComm] = finalStr
+
+	if _, ok := a.realDests[d.ranksRealComm]; ok {
+		a.realDests[d.ranksRealComm]++
+	} else {
+		a.realDests[d.ranksRealComm] = 1
+	}
+
+	return true, nil
+}
+
+func analyzeSendCounts(input string, outputDir string) error {
+	parsing := true // used to track if we are actively parsing data or just skipping text
+	a := createAnalyzer()
+	rank := 0
+	alltoallvCallNumber := -1
+	alltoallvCallStart := 0
+	lineNumber := 1
+
+	file, err := os.Open(input)
+	if err != nil {
+		return fmt.Errorf("unable to open %s: %w", input, err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			for key, _ := range a.realDests {
+				fmt.Printf("alltoallv call #%d: %d ranks communicate with %d other ranks (%s)\n", alltoallvCallNumber+alltoallvCallStart, a.realDests[key], key, a.ranksComm[key])
+			}
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		if strings.HasPrefix(line, "### Raw") {
+			parsing = false
+		}
+
+		if strings.HasPrefix(line, "Alltoallv calls") {
+			callRange := strings.ReplaceAll(line, "Alltoallv calls ", "")
+			tokens := strings.Split(callRange, "-")
+			if len(tokens) == 2 {
+				alltoallvCallStart, err = strconv.Atoi(tokens[0])
+				if err != nil {
+					return nil
+				}
+			}
+		}
+
+		if line != "" && parsing == true {
+			res, err := a.parseLine(rank, lineNumber, line)
+			if err != nil {
+				return err
+			}
+			if res {
+				rank++
+			}
+		}
+
+		if lineNumber > 1 && strings.HasPrefix(line, "### Raw") {
+			for key, _ := range a.realDests {
+				fmt.Printf("alltoallv call #%d: %d ranks communicate with %d other ranks (%s)\n", alltoallvCallNumber+alltoallvCallStart, a.realDests[key], key, a.ranksComm[key])
+			}
+		}
+
+		if strings.HasPrefix(line, "BEGIN") {
+			parsing = true
+			rank = 0
+			alltoallvCallNumber++
+			a.resetStats()
+		}
+
+		lineNumber++
+	}
+
+	a.finalize()
+
+	return nil
+}
+
 func main() {
-	
+	file := flag.String("file", "", "Path to the file from which we want to extract counters")
+	outputDir := flag.String("output-dir", "", "Where the output files will be stored")
+
+	flag.Parse()
+
+	if *file == "" || *outputDir == "" {
+		log.Fatalf("undefined input file or output directory")
+	}
+
+	err := analyzeSendCounts(*file, *outputDir)
+	if err != nil {
+		log.Fatalf("[ERROR] Impossible to analyze send counters: %s", err)
+	}
 }
