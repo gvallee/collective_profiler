4bb4a35bf33a2229868f7d6859b62de53aae88b0
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2020-05-18 17:08:51 -0400

    Fix #1 - Add tool to get counters for a specific rank and call
    Signed-off-by: Geoffroy Vallee <geoffroy.vallee@gmail.com>

---
+++ tools/cmd/getcounters/getcounters.go
@@ -0,0 +1,48 @@
+//
+// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+//
+// See LICENSE.txt for license information
+//
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/gvallee/alltoallv_profiling/tools/internal/pkg/profiler"
+	"github.com/gvallee/go_util/pkg/util"
+)
+
+func main() {
+	verbose := flag.Bool("v", false, "Enable verbose mode")
+	rank := flag.Int("rank", 0, "Rank for which we want to extract counters")
+	call := flag.Int("call", 0, "Number of the alltoallv call for which we want to extract counters")
+	dir := flag.String("dir", "", "Where the data files are stored")
+	id := flag.Int("id", 0, "Identifier of the experiment, e.g., X from <pidX> in the profile file name")
+
+	flag.Parse()
+
+	logFile := util.OpenLogFile("alltoallv", "getcounters")
+	defer logFile.Close()
+	if *verbose {
+		nultiWriters := io.MultiWriter(os.Stdout, logFile)
+		log.SetOutput(nultiWriters)
+	} else {
+		log.SetOutput(ioutil.Discard)
+	}
+
+	sendCounters, recvCounters, err := profiler.FindCounters(*dir, *id, *rank, *call)
+	if err != nil {
+		log.Fatalf("unable to find counters: %s", err)
+	}
+
+	//fmt.Println("Send counters:")
+	fmt.Printf("%s", sendCounters)
+	//fmt.Println("Receive counters")
+	fmt.Printf("%s\n", recvCounters)
+}
--- tools/cmd/recvprofiler/recvprofiler.go
+++ tools/cmd/recvprofiler/recvprofiler.go
@@ -8,21 +8,35 @@ package main
 
 import (
 	"flag"
+	"io"
+	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/gvallee/alltoallv_profiling/tools/internal/pkg/profiler"
+	"github.com/gvallee/go_util/pkg/util"
 )
 
 func main() {
+	verbose := flag.Bool("v", false, "Enable verbose mode")
 	file := flag.String("file", "", "Path to the file from which we want to extract counters")
 
 	flag.Parse()
+
+	logFile := util.OpenLogFile("alltoallv", "recvprofiler")
+	defer logFile.Close()
+	if *verbose {
+		nultiWriters := io.MultiWriter(os.Stdout, logFile)
+		log.SetOutput(nultiWriters)
+	} else {
+		log.SetOutput(ioutil.Discard)
+	}
 
 	if *file == "" {
 		log.Fatalf("undefined input file or output directory")
 	}
 
-	err := profiler.HandleCounts(*file)
+	err := profiler.HandleCounters(*file)
 	if err != nil {
 		log.Fatalf("[ERROR] Impossible to analyze recv counters: %s", err)
 	}
