e9af484820f3652db5dde3f74d5a3f347db52855
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2020-06-03 16:44:03 -0400

    Switch away from c99
    Signed-off-by: Geoffroy Vallee <geoffroy.vallee@gmail.com>

--- examples/alltoallv.c
+++ examples/alltoallv.c
@@ -8,16 +8,19 @@
 #include <stdio.h>
 #include "mpi.h"
 
-#define MPICHECK(c) do {                         \
-    if (c != MPI_SUCCESS) {                      \
-        fprintf(stderr, "MPI command failed\n"); \
-        return 1;                                \
-    }                                            \
-} while(0);
+#define MPICHECK(c)                                  \
+    do                                               \
+    {                                                \
+        if (c != MPI_SUCCESS)                        \
+        {                                            \
+            fprintf(stderr, "MPI command failed\n"); \
+            return 1;                                \
+        }                                            \
+    } while (0);
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
+    int i;
     int world_size;
     int world_rank;
     int *send_buffer;
@@ -31,22 +34,25 @@ main(int argc, char **argv)
     MPICHECK(MPI_Comm_size(MPI_COMM_WORLD, &world_size));
     MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &world_rank));
 
-    send_buffer = (int*)calloc(world_size * world_size, sizeof(int));
-    recv_buffer = (int*)calloc(world_size * world_size, sizeof(int));
+    send_buffer = (int *)calloc(world_size * world_size, sizeof(int));
+    recv_buffer = (int *)calloc(world_size * world_size, sizeof(int));
     send_count = calloc(world_size, sizeof(int));
     recv_count = calloc(world_size, sizeof(int));
     send_displ = calloc(world_size, sizeof(int));
     recv_displ = calloc(world_size, sizeof(int));
-    if (!send_buffer || !recv_buffer || !send_count || !recv_count || !send_displ || !recv_displ) {
+    if (!send_buffer || !recv_buffer || !send_count || !recv_count || !send_displ || !recv_displ)
+    {
         fprintf(stderr, "Out of resources\n");
         goto exit_on_failure;
     }
 
-    for (int i = 0; i < world_size * world_size; i++) {
+    for (i = 0; i < world_size * world_size; i++)
+    {
         send_buffer[i] = i + 10 * world_rank;
     }
 
-    for (int i = 0; i < world_size; i++) {
+    for (i = 0; i < world_size; i++)
+    {
         send_count[i] = i;
         recv_count[i] = world_rank;
         recv_displ[i] = i * world_rank;
