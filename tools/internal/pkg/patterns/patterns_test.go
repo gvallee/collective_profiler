378801c7d9e70e99c77b0a0a9b7bf5517936a598
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2020-07-24 11:54:23 -0400

    More tests for patterns
    Signed-off-by: Geoffroy Vallee <geoffroy.vallee@gmail.com>

--- tools/internal/pkg/patterns/patterns_test.go
+++ tools/internal/pkg/patterns/patterns_test.go
@@ -14,6 +14,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"testing"
 
 	"github.com/gvallee/alltoallv_profiling/tools/internal/pkg/counts"
@@ -39,9 +40,26 @@ func compareFiles(t *testing.T, file1 string, file2 string) {
 	_, err = io.Copy(hasher, file2Fd)
 	hashFile2 := hex.EncodeToString(hasher.Sum(nil))
 
-	if hashFile2 != hashFile2 {
+	if hashFile1 != hashFile2 {
 		t.Fatalf("resulting file is not as expected: hashes are %s and %s (%s %s)", hashFile1, hashFile2, file1, file2)
 	}
+}
+
+func generateCounts(rankStart int, rankEnd int, numZeros int, numNonZeros int, value int) []string {
+	str := "Rank(s) " + strconv.Itoa(rankStart) + "-" + strconv.Itoa(rankEnd) + ":"
+	if rankStart == rankEnd {
+		str = "Rank(s) " + strconv.Itoa(rankStart) + ":"
+	}
+
+	for i := 0; i < numZeros; i++ {
+		str += " 0"
+	}
+
+	for i := 0; i < numNonZeros; i++ {
+		str += " " + strconv.Itoa(value)
+	}
+
+	return []string{str}
 }
 
 func TestParsingCounts(t *testing.T) {
@@ -54,6 +72,16 @@ func TestParsingCounts(t *testing.T) {
 			name:       "oneRankSend1To10Recv0",
 			sendCounts: []string{"Rank(s) 0: 1 2 3 4 5 6 7 8 9 10"},
 			recvCounts: []string{"Rank(s) 0: 1 0 0 0 0 0 0 0 0 0"},
+		},
+		{
+			name:       "threeRanksSendNToNRecvNToN",
+			sendCounts: []string{"Rank(s) 0-2: 3 3 3"},
+			recvCounts: []string{"Rank(s) 0-2: 1 1 1"},
+		},
+		{
+			name:       "threeRanksNto1",
+			sendCounts: generateCounts(0, 109, 109, 1, 3),                                              // Ranks 0-109: 109 zero counts and one count equal to 3
+			recvCounts: append(generateCounts(0, 0, 1, 110, 42), generateCounts(1, 109, 110, 0, 0)...), //Rank 0: 1 count equal to 0 and 110 counts equal to 42; ranks 1-109:  110 zero counts, non counts with non-zero vaue
 		},
 	}
 
