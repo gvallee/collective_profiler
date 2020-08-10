4cc944948d3a7531c13f7b525b0bb464952ba0b4
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2020-08-10 11:13:06 -0400

    webui: diplay send/recv counts of selected calls
    Signed-off-by: Geoffroy Vallee <geoffroy.vallee@gmail.com>

--- tools/cmd/webui/webui.go
+++ tools/cmd/webui/webui.go
@@ -16,6 +16,8 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
+	"strings"
 	"text/template"
 
 	"github.com/gvallee/alltoallv_profiling/tools/internal/pkg/bins"
@@ -29,27 +31,103 @@ type CallsPageData struct {
 	Calls     []counts.CommDataT
 }
 
-func displayUI(datasetBasedir string, datasetName string) error {
+type CallPageData struct {
+	LeadRank  int
+	CallID    int
+	CallsData []counts.CommDataT
+}
+
+var datasetBasedir string
+var datasetName string
+var mainData CallsPageData
+
+var basedir string
+
+func CallHandler(w http.ResponseWriter, r *http.Request) {
+	var err error
+
+	leadRank := 0
+	callID := 0
+	params := r.URL.Query()
+	for k, v := range params {
+		if k == "leadRank" {
+			leadRank, err = strconv.Atoi(v[0])
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}
+
+		if k == "callID" {
+			callID, err = strconv.Atoi(v[0])
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}
+	}
+
+	cpd := CallPageData{
+		LeadRank:  leadRank,
+		CallID:    callID,
+		CallsData: mainData.Calls,
+	}
+
+	callTemplate, err := template.New("callDetails.html").Funcs(template.FuncMap{
+		"displaySendCounts": func(cd []counts.CommDataT, leadRank int, callID int) string {
+			for _, data := range cd {
+				if data.LeadRank == leadRank {
+					return strings.Join(cd[leadRank].CallData[callID].SendData.RawCounts, "<br />")
+				}
+			}
+			return "Call not found"
+		},
+		"displayRecvCounts": func(cd []counts.CommDataT, leadRank int, callID int) string {
+			for _, data := range cd {
+				if data.LeadRank == leadRank {
+					return strings.Join(cd[leadRank].CallData[callID].RecvData.RawCounts, "<br />")
+				}
+			}
+			return "Call not found"
+		},
+	}).ParseFiles(filepath.Join(basedir, "templates", "callDetails.html"))
+
+	err = callTemplate.Execute(w, cpd)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
 	sizeThreshold := 200
 	binThresholds := "200,1024,2048,4096"
 	listBins := bins.GetFromInputDescr(binThresholds)
 	_, _, _, allCallsData, err := profiler.HandleCountsFiles(datasetBasedir, sizeThreshold, listBins)
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
-	data := CallsPageData{
+	mainData = CallsPageData{
 		PageTitle: datasetName,
 		Calls:     allCallsData,
 	}
 
-	_, filename, _, _ := runtime.Caller(0)
-	basedir := filepath.Dir(filename)
+	indexTemplate, err := template.New("callsLayout.html").ParseFiles(filepath.Join(basedir, "templates", "callsLayout.html"))
+	err = indexTemplate.Execute(w, mainData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
-	tmpl := template.Must(template.ParseFiles(filepath.Join(basedir, "templates", "callsLayout.html")))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, data)
-	})
+func displayUI(dataBasedir string, name string) error {
+	datasetBasedir = dataBasedir
+	datasetName = name
+	_, filename, _, _ := runtime.Caller(0)
+	basedir = filepath.Dir(filename)
+
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/call", CallHandler)
 	http.ListenAndServe(":8080", nil)
 
 	return nil
