998666f61e598eb4bf2a16817edab58cd6d8ffca
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2021-02-03 02:40:31 -0500

    Fix a bug in a scaling function

--- tools/internal/pkg/scale/scale.go
+++ tools/internal/pkg/scale/scale.go
@@ -28,14 +28,16 @@ func allZerosInts(sortedValues []int) bool {
 
 func allZerosFloat64s(sortedValues []float64) bool {
 	// If all values are 0 nothing can be done
-	if len(sortedValues) >= 2 {
-		if sortedValues[0] == 0 && sortedValues[len(sortedValues)-1] == 0 {
-			return true
-		}
-	} else {
-		if sortedValues[0] == 0 {
-			return true
-		}
+	if len(sortedValues) >= 2 && sortedValues[0] == 0 && sortedValues[len(sortedValues)-1] == 0 {
+		return true
+	}
+
+	if len(sortedValues) == 1 && sortedValues[0] == 0 {
+		return true
+	}
+
+	if len(sortedValues) == 0 {
+		return true
 	}
 
 	return false
