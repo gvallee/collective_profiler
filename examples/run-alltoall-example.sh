f9b78526d62e32ddb9deab9d4048589455a09ebb
Cyrus James Legg <j.legg.17@ucl.ac.uk>
2021-02-23 06:05:14 -0800

    add run-alltoall-example.sh
    to test running and allow generation of output reference files for tests
    
    Signed-off-by: Cyrus James Legg <j.legg.17@ucl.ac.uk>

---
+++ examples/run-alltoall-example.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# module for HPCAC
+spack unload --all
+module purge
+module load gcc/8.3.1 hpcx/2.7.0
+
+# profiling environment variables
+export JOB_NOW=$( date +%Y%m%d-%H%M%S )
+export PROJECT_ROOT=/global/home/users/cyrusl/placement/expt0070/alltoall_profiling
+export A2A_PROFILING_OUTPUT_DIR=$PROJECT_ROOT/examples/results/run-at-${JOB_NOW}
+
+MPIFLAGS="--mca pml ucx -x UCX_NET_DEVICES=mlx5_1:1 "
+MPIFLAGS+="-x A2A_PROFILING_OUTPUT_DIR "
+MPIFLAGS+="-x LD_LIBRARY_PATH "
+
+EXAMPLE_PROG=alltoall_simple_c
+
+# the alltoall program 
+mpirun -np 4 \
+        $MPIFLAGS \
+       -x LD_PRELOAD=$PROJECT_ROOT/src/alltoall/liballtoall_counts.so \
+       $PROJECT_ROOT/examples/$EXAMPLE_PROG
