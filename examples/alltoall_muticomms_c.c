fd797717f68f669d282ffa4102a535ebd8cdd12f
Cyrus James Legg <j.legg.17@ucl.ac.uk>
2021-02-24 08:32:17 -0800

    add dt and multicomms tests to alltoall examples
    Signed-off-by: Cyrus James Legg <j.legg.17@ucl.ac.uk>

--- examples/alltoall_muticomms_c.c
+++ examples/alltoall_muticomms_c.c
@@ -4,8 +4,8 @@ A simple test of MPI_Alltoall, just one call to that
 
 #include "alltoall_test_helpers.h"
 
-#define PARAM_SETS_COUNT 1
-#define RANK_SETS_COUNT 1
+#define PARAM_SETS_COUNT 9
+#define RANK_SETS_COUNT 9
 
 rank_set_t* create_rank_sets(){
     rank_set_t* rank_sets = malloc(sizeof(rank_set_t) * RANK_SETS_COUNT);  
@@ -32,7 +32,15 @@ rank_set_t* create_rank_sets(){
 
 alltoall_test_node_params_t* create_params_sets(rank_set_t* rank_sets){
     alltoall_test_node_params_t* params_sets = (alltoall_test_node_params_t*) malloc(sizeof(alltoall_test_node_params_t) * PARAM_SETS_COUNT);
-    params_sets[0] = (alltoall_test_node_params_t) {.send_type_idx = 2, .recv_type_idx = 2, .sendcount =  16, .recvcount =  16, .rank_set = &rank_sets[0], .repetitions=1000000};
+    params_sets[0] = (alltoall_test_node_params_t) {.send_type_idx = 2, .recv_type_idx = 2, .sendcount =  16, .recvcount =  16, .rank_set = &rank_sets[0], .repetitions=1};
+    params_sets[1] = (alltoall_test_node_params_t) {.send_type_idx = 2, .recv_type_idx = 2, .sendcount =  16, .recvcount =  16, .rank_set = &rank_sets[1], .repetitions=1};
+    params_sets[2] = (alltoall_test_node_params_t) {.send_type_idx = 2, .recv_type_idx = 2, .sendcount =  16, .recvcount =  16, .rank_set = &rank_sets[2], .repetitions=1};
+    params_sets[3] = (alltoall_test_node_params_t) {.send_type_idx = 2, .recv_type_idx = 2, .sendcount =  16, .recvcount =  16, .rank_set = &rank_sets[3], .repetitions=1};
+    params_sets[4] = (alltoall_test_node_params_t) {.send_type_idx = 2, .recv_type_idx = 2, .sendcount =  16, .recvcount =  16, .rank_set = &rank_sets[4], .repetitions=1};
+    params_sets[5] = (alltoall_test_node_params_t) {.send_type_idx = 2, .recv_type_idx = 2, .sendcount =  16, .recvcount =  16, .rank_set = &rank_sets[5], .repetitions=1};
+    params_sets[6] = (alltoall_test_node_params_t) {.send_type_idx = 2, .recv_type_idx = 2, .sendcount =  16, .recvcount =  16, .rank_set = &rank_sets[6], .repetitions=1};
+    params_sets[7] = (alltoall_test_node_params_t) {.send_type_idx = 2, .recv_type_idx = 2, .sendcount =  16, .recvcount =  16, .rank_set = &rank_sets[7], .repetitions=1};
+    params_sets[8] = (alltoall_test_node_params_t) {.send_type_idx = 2, .recv_type_idx = 2, .sendcount =  16, .recvcount =  16, .rank_set = &rank_sets[8], .repetitions=1};
     return params_sets;
 } 
 
@@ -51,8 +59,8 @@ int main(int argc, char *argv[]) {
     alltoall_test_node_params_t* param_sets = create_params_sets(rank_sets);
 
     /* to test aggregation of patterns this set should have duplicates */
-    int param_sets_set_count = 1;
-    int param_sets_indices[] = {0};
+    int param_sets_set_count = 9;
+    int param_sets_indices[] = {0, 1, 2, 3, 4, 5,6 ,7, 8};
     
     do_test(param_sets, param_sets_set_count, param_sets_indices, my_rank);
 
