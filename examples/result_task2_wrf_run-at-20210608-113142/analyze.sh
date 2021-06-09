2b6b31f9efae2aca05111240d63f307c3df46cc7
yangyw <yangyw@shanghaitech.edu.cn>
2021-06-09 19:54:04 +0800

    dir modification

--- examples/result_task2_wrf_run-at-20210608-113142/analyze.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# somewhere to keep the results of post processing the results of the cluster job
-export RESULTS_ROOT=$( dirname $(readlink --canonicalize --no-newline "$0" ) )
-export POST_ANALYSYS_ROOT="$RESULTS_DIR/post_processed"
-echo "this script is as yet a dummy and has set only some paths - no analysis performed
-# TODO call some post processing scripts
-# TODO copy the post processing scripts to the post processing directory for a record copy
-# TODO set results of postprocessing to read only
-# TODO test all this including the exports above
