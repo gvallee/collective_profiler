bdd7ed5b1fe5d3cc2a34c725a28e89c61910a868
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2022-11-09 08:41:55 -0500

    Improve the allgatherv test

--- examples/allgatherv.c
+++ examples/allgatherv.c
@@ -18,44 +18,69 @@
         }                                            \
     } while (0);
 
-int main(int argc, char **argv)
+static int run_allgatherv(int num_elts, int world_size)
 {
-    int i;
-    int world_size;
-    int world_rank;
-    int send_buffer;
-    int send_count = 1;
+    size_t idx = 0;
+    int i, j;
+    int *send_buffer;
+    int send_count = num_elts;
     int *recv_buffer;
-    int *recv_count;
-    int *recv_displ;
+    int *recv_counts;
+    int *recv_displs;
 
-    MPICHECK(MPI_Init(&argc, &argv));
-    MPICHECK(MPI_Comm_size(MPI_COMM_WORLD, &world_size));
-    MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &world_rank));
-    send_buffer = world_rank;
-
-    recv_buffer = (int *)calloc(world_size, sizeof(int));
-    recv_count = calloc(world_size, sizeof(int));
-    recv_displ = calloc(world_size, sizeof(int));
-    if (!recv_buffer || !recv_count || !recv_displ)
+    send_buffer = (int *)calloc(num_elts, sizeof(int));
+    recv_buffer = (int *)calloc(num_elts * world_size, sizeof(int));
+    recv_counts = (int *)calloc(world_size, sizeof(int));
+    recv_displs = (int *)calloc(world_size, sizeof(int));
+    if (!send_buffer || !recv_buffer || !recv_counts || !recv_displs)
     {
         fprintf(stderr, "Out of resources\n");
         goto exit_on_failure;
     }
 
+    for (i = 0; i < num_elts; i++)
+        send_buffer[i] = i;
+
     for (i = 0; i < world_size; i++)
     {
-        recv_count[i] = 1;
-        recv_displ[i] = i;
+        recv_counts[i] = num_elts;
+        recv_displs[i] = i + num_elts;
     }
 
-    MPICHECK(MPI_Allgatherv(&send_buffer, send_count, MPI_INT,
-                            recv_buffer, recv_count, recv_displ, MPI_INT,
+    MPICHECK(MPI_Allgatherv(send_buffer, send_count, MPI_INT,
+                            recv_buffer, recv_counts, recv_displs, MPI_INT,
                             MPI_COMM_WORLD));
-
+    free(send_buffer);
     free(recv_buffer);
-    free(recv_count);
-    free(recv_displ);
+    free(recv_counts);
+    free(recv_displs);
+    return 0;
+
+exit_on_failure:
+    free(send_buffer);
+    free(recv_buffer);
+    free(recv_counts);
+    free(recv_displs);
+    return -1;
+}
+
+int main(int argc, char **argv)
+{
+    int rc, num_elts;
+    int world_size;
+    int world_rank;
+
+    MPICHECK(MPI_Init(&argc, &argv));
+    MPICHECK(MPI_Comm_size(MPI_COMM_WORLD, &world_size));
+    MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &world_rank));
+
+    for (num_elts = 1; num_elts < 3; num_elts++)
+    {
+        rc = run_allgatherv(num_elts, world_size);
+        if (rc)
+            goto exit_on_failure;
+    }
+
     MPI_Finalize();
     return EXIT_SUCCESS;
 
